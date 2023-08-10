dd47f518327c0fc47d850e19d42410539aeb6a2c
Muhammad Wasif <115789986+mwasif666@users.noreply.github.com>
2023-08-10 15:02:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// OWL CARDS
+$(".cardss").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      100: {
+        items: 1,
+      },
+    },
+  });
+  $(".owl-prev").html('<i class="bi bi-arrow-left-square-fill icon"></i>');
+  $(".owl-next").html('<i class="bi bi-arrow-right-square-fill icon"></i>');
+
+//   Products
+
